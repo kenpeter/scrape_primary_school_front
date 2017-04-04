cba67cc0b827bd83012322386fe09071bbb42168
kenpeter <figo2476@hotmail.com>
2017-04-04 16:27:43 +1000

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+## Intro
+
+* https://github.com/istarkov/google-map-react-examples/blob/master/web/flux/components/examples/x_events/events_map_page.jsx
+
+* https://swizec.com/blog/mobx-with-create-react-app/swizec/7158
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+// watcher
+import { observer } from 'mobx-react';
+
+import School from './components/School';
+
+import './App.css';
+
+@observer
+class App extends Component {
+  static defaultProps = {
+    center: {lat: -37.8136, lng: 144.9631},
+    zoom: 11
+  };
+
+  render() {
+    let schoolDisplay;
+
+    if(!this.props.data.is_loading) {
+      console.log(this.props.data.schools);
+
+      schoolDisplay = this.props.data.schools.map( (school, index) => {
+        let mytext = school.schoolName + ' ' + school.medianScore + ' ' + school.top40;
+        return (
+          <School
+            key={index}
+            lat={school.latLng.lat}
+            lng={school.latLng.lng}
+            text={mytext}
+          />
+        )
+      });
+    }
+    else {
+
+    }
+
+    //console.log('-- test --');
+    //console.log(schoolDisplay);
+
+    return (
+      <div className="App">
+        <GoogleMapReact
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          { schoolDisplay }
+        </GoogleMapReact>
+
+        <div className="explain">
+          <p>test....</p>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
